30dc1dac0bef8440a2bba71505030840d0b68289
James R. Lambert <ryolambert@gmail.com>
2018-08-12 17:24:25 -0600

    started files and first functions.

---
+++ script.js
@@ -0,0 +1,7 @@
+function highlightMouse() {
+	document.getElementById("highlight").style.backgroundColor = 'yellow';
+}
+
+function changeFontFamily() {
+	document.getElementById("highlight").style.fontFamily = 'Monospace';
+}
