fa2dea36f9d6e193768fd13920d7cd4bd9280481
James R. Lambert <ryolambert@gmail.com>
2018-08-13 13:48:57 -0600

    fixed positioning, added word search highlighter, and added Json fetch.

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
 function highlightMouse() {
 	document.getElementById("highlight").style.backgroundColor = 'yellow';
+	document.getElementById("highlight").style.fontFamily = 'Monospace';
 }
 
-function changeFontFamily() {
-	document.getElementById("highlight").style.fontFamily = 'Monospace';
+function unhighlightMouse() {
+	document.getElementById("highlight").style.backgroundColor = 'white';
+	document.getElementById("highlight").style.fontFamily = 'Sans-Serif';
+}
+
+/**********************************
+ Highlighted Search
+ **********************************/
+document.addEventListener('DOMContentLoaded', function() {
+	var searchInput = document.getElementById("inputSearch").innerHTML;
+	searchInput = searchInput.toString();
+	document.getElementById("search").onclick = function() {
+		highlight_word(searchInput)
+	};
+}, false);
+
+function highlight_word(searchInput) {
+	var text = document.getElementById("search_text").value;
+	if(text) {
+		var pattern = new RegExp("(" + text + ")", "gi");
+		var new_text = searchInput.replace(pattern, "<span class='highlight'>" + text + "</span>");
+		document.getElementById("inputSearch").innerHTML = new_text;
+	}
+}
+
+/* Fetch Request to JsonPlaceholder */
+function fetchJson() {
+	fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+		.then(res => res.json())
+		.then((data) => {
+			let output = '<h4> Posts </h4>';
+			data.forEach(function(user) {
+				output += `
+			<ul>
+				<li>ID: ${user.id}</li>
+				<li>Title: ${user.title}</li>
+				<li>Body: ${user.body}</li>
+			</ul>
+			`;
+				document.getElementById('fetchOutput').innerHTML = output
+			});
+		});
 }
